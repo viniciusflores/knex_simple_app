adbf6044ffe96d22f80856a8519eaa17c11b6528
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-09 18:59:59 -0300

    :sparkles: Complete database V1

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
+import { env } from './env'
 import { knex as setupKnex, Knex } from 'knex'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,6 +13,32 @@ app.get('/hello', async () => {
   return `Hello World`
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.post('/transactions', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de Teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app.get('/transactions', async () => {
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
+})
+
+app.get('/transactions/123', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 500)
+    .select('*')
+
+  return transactions
+})
+
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running in port 3333! 🚀')
 })
