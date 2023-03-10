384e86ee9b5987dacc3d620ac038fae101ca6206
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-10 13:33:29 -0300

    :white_check_mark: Include get test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,3 +1,4 @@
+import { cookie } from '@fastify/cookie'
 import { test, expect, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
@@ -19,5 +20,30 @@ describe('Transactions routes', () => {
     })
 
     expect(response.statusCode).toEqual(201)
+  })
+  test('Create a interaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction to get',
+        amount: 500,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        id: expect.any(String),
+        title: 'New Transaction to get',
+        amount: 500,
+      }),
+    ])
   })
 })
