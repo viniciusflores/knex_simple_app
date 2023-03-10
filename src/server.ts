ccc96de0ad71607222982b489fdbce5d46fc45b5
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-10 10:57:51 -0300

    :construction: start with :cookie:

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import cookie from '@fastify/cookie'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions.routes'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  console.log(tables)
-
+  console.log('Hello route is called')
   return `Hello World`
 })
 
