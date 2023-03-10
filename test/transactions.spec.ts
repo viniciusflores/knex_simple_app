64f4db4049bad11be42625aef7cf54deae82574f
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-10 13:22:19 -0300

    :truck: rename test file

---
+++ test/transactions.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('Create a interaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
