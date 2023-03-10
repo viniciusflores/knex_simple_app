b6bb798629b065b9b8b8984bb0bafde26e384043
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-10 10:27:38 -0300

    :sparkles: summary route

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -42,4 +42,12 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return { transaction }
   })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
 }
